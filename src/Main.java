7f4c878aeeb183f73adbe78bb2338aaffc724b2a
Ismail Magdy <92678464+ismailmagdyismail@users.noreply.github.com>
2023-11-10 19:02:52 +0200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,15 @@
 
-// TODO add Array of algo for UI
-// TODO FIle read ,write
+
+import CompressView.CompressionView;
+import Compression.LZW.LZWCompression;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        App.run();
+//        new CompressionView();
+//        System.out.println(new LZWCompression().compress("ismail"));
+//        System.out.println(new LZWCompression().compress("ismailismailismailismail"));
+        System.out.println(new LZWCompression().decompress("[<9>,<19>,<13>,<1>,<9>,<12>,<53>,<55>,<57>,<59>,<56>,<58>,<54>,<63>]"));
     }
 }
-
-/*
-compress tests:
-    test 1#:
-        input:
-            cabracadabrarrarrad
-        output:
-            <0, 0, c>
-            <0, 0, a>
-            <0, 0, b>
-            <0, 0, r>
-            <3, 1, c>
-            <2, 1, d>
-            <7, 4, r>
-            <3, 3, r>
-            <3, 1, d>
-
-    test 2#:
-        input:
-            abracadabrad
-        output:
-            <0, 0, a>
-            <0, 0, b>
-            <0, 0, r>
-            <3, 1, c>
-            <2, 1, d>
-            <7, 4, d>
-
-    test 3#:
-        input:
-            ABAABABAABBBBBBBBBBBBA
-        output:
-            <0, 0, A>
-            <0, 0, B>
-            <2, 1, A>
-            <3, 2, B>
-            <5, 3, B>
-            <2, 2, B>
-            <5, 5, B>
-            <1, 1, A>
-*/
